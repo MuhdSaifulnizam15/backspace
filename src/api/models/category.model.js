51371812c1f754fce5a778ac683c3b12daaedd68
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2021-07-25 00:18:34 +0800

    fix: added category id into category model

--- src/api/models/category.model.js
+++ src/api/models/category.model.js
@@ -7,7 +7,13 @@ const categorySchema = mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-    }
+    },
+    category_id: {
+        type: Number,
+        required: true,
+        index: true,
+        unique: true
+    },
 });
 
 categorySchema.plugin(toJSON);
@@ -15,6 +21,11 @@ categorySchema.plugin(mongoosePaginate);
 
 categorySchema.statics.isNameTaken = async function (name, excludeCategoryId) {
     const category = await this.findOne({ name, _id: { $ne: excludeCategoryId }});
+    return !!category;
+};
+
+categorySchema.statics.isIdTaken = async function (category_id, excludeCategoryId) {
+    const category = await this.findOne({ category_id, _id: { $ne: excludeCategoryId } });
     return !!category;
 };
 
